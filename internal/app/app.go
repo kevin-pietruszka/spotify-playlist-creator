f448dfb7900dd06c7ab3144b2535bcc135ad023b
Kevin Pietruszka <kevinpiet13@gmail.com>
2024-12-01 10:09:16 -0500

    textinput support

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
@@ -11,6 +12,9 @@ import (
 type model struct {
 	screenHeight int
 	screenWidth  int
+	leftWidth    int
+	rightWidth   int
+	textInput    textinput.Model
 }
 
 type tickMsg time.Time
@@ -18,11 +22,17 @@ type tickMsg time.Time
 const spotifyGreen = "#008000"
 
 func initialModel() model {
-	return model{}
+	ti := textinput.New()
+	ti.Placeholder = "Type to look for artists, playlist, etc."
+	ti.Focus()
+	ti.CharLimit = 128
+	return model{
+		textInput: ti,
+	}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
@@ -30,6 +40,11 @@ func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.screenHeight = msg.Height
 		m.screenWidth = msg.Width
+
+		m.rightWidth = m.screenWidth / 3
+		m.leftWidth = m.screenWidth - m.rightWidth
+
+		m.textInput.Width = m.leftWidth - 2 - 2 - 1
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc", "ctrl+c":
@@ -37,7 +52,10 @@ func (m model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	}
 
-	return m, nil
+	var cmd tea.Cmd
+	m.textInput, cmd = m.textInput.Update(message)
+
+	return m, cmd
 }
 
 func (m model) View() string {
@@ -49,10 +67,8 @@ func (m model) View() string {
 		return fullscreen.Render("Expand your screen")
 	}
 
-	rightWidth := m.screenWidth / 3
-	leftWidth := m.screenWidth - rightWidth
-	leftWidthWithBorder := leftWidth - 2
-	rightWidthWithBorder := rightWidth - 2
+	leftWidthWithBorder := m.leftWidth - 2
+	rightWidthWithBorder := m.rightWidth - 2
 
 	borderStyle := lipgloss.NewStyle().
 		Border(lipgloss.RoundedBorder()).
@@ -78,7 +94,7 @@ func (m model) View() string {
 		Width(leftWidthWithBorder).
 		Height(resultsHeight)
 
-	leftPanel := lipgloss.JoinVertical(lipgloss.Top, resultsBox.Render("Results"), searchBox.Render("Search"))
+	leftPanel := lipgloss.JoinVertical(lipgloss.Top, resultsBox.Render("Results"), searchBox.Render(m.textInput.View()))
 	rightPanel := lipgloss.JoinVertical(lipgloss.Top, listBox.Render("Added List"), descriptionBox.Render("Description"))
 
 	return lipgloss.JoinHorizontal(lipgloss.Top, leftPanel, rightPanel)
